c922cb46e363dcb068696658bc5204897eb3ef64
azmera123 <ajmeeranikitha3@gmail.com>
2025-10-22 11:38:24 +0530

    adding two files

--- samplename.java
+++ samplename.java
@@ -1,6 +1,6 @@
 public class samplename {
     public static void main(String[] args) {
-        String name="spandana";
+        String name="nikitha";
 
         System.out.println("hello ,my name is " + name);
     }
